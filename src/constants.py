be903e1d1d0071985684e2f07ec88c577d1b8273
cdolly <72312670+cdolly@users.noreply.github.com>
2022-06-13 13:13:14 -0400

    Create constants.py

---
+++ src/constants.py
@@ -0,0 +1,22 @@
+# test file filename
+FILENAME = 'src/tests/Test/identifiers.txt'
+
+# regex
+REGEX_CHAR = "[a-zA-Z$]"
+REGEX_NUM = "[0-9]"
+REGEX_WHITESPACE = "\s"
+
+# characters
+DECIMAL = "."
+EXPONENT = "e"
+
+# positions in hash table
+EOL = "34"
+EOF = "35"
+
+# max/min
+IDENTIFIER_MAX = 30
+NUMERAL_MAX = 10
+
+WARNING = "[WARNING]:"
+INVALID_TOKEN = '{INVALID_TOKEN}'
